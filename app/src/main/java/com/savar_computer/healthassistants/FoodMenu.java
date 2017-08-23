d7600b59e3d3e30db02227197fddf71080f3475b
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 18:11:05 +0430

    version 18

---
+++ app/src/main/java/com/savar_computer/healthassistants/FoodMenu.java
@@ -0,0 +1,13 @@
+package com.savar_computer.healthassistants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food_menu);
+    }
+}
