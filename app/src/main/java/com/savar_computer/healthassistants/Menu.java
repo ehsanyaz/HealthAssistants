dd264aaa1344dcf2ddbb57102170a31c62660dff
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 13:11:16 +0430

    version 8

--- app/src/main/java/com/savar_computer/healthassistants/Menu.java
+++ app/src/main/java/com/savar_computer/healthassistants/Menu.java
@@ -13,7 +13,5 @@ public class Menu extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-        stepBar=(RelativeLayout)findViewById(R.id.menu_progressStep_bar);
-
     }
 }
