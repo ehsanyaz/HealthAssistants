8fe92eefc58e24a886f8bd580142c0945055e01d
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 13:48:36 +0430

    version 12

--- app/src/main/java/com/savar_computer/healthassistants/Menu.java
+++ app/src/main/java/com/savar_computer/healthassistants/Menu.java
@@ -2,17 +2,19 @@ package com.savar_computer.healthassistants;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 public class Menu extends Activity {
 
-    private RelativeLayout stepBar;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
-
+        progressBar.setMax(100);
+        progressBar.setProgress(50);
     }
 }
