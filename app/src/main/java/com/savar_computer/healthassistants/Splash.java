995c8f634784d6346d762d55864be038cbf21d11
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 10:54:08 +0430

    version 2

--- app/src/main/java/com/savar_computer/healthassistants/Splash.java
+++ app/src/main/java/com/savar_computer/healthassistants/Splash.java
@@ -1,13 +1,43 @@
 package com.savar_computer.healthassistants;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
+
+    private Handler handler;
+
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sound);
+        mediaPlayer.start();
+
+        txt = (TextView) findViewById(R.id.splash_txtview);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Font/font.ttf");
+        txt.setTypeface(typeface);
+
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Profile.class);
+                startActivity(intent);
+                mediaPlayer.stop();
+                finish();
+            }
+        }, 2000);
+
+
     }
 }
