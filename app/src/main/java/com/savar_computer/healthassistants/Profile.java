14e7bfd011fb37ce0c03cf93e8a22f756db7eb81
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 10:45:26 +0430

    version 2

--- app/src/main/java/com/savar_computer/healthassistants/Profile.java
+++ app/src/main/java/com/savar_computer/healthassistants/Profile.java
@@ -1,9 +1,9 @@
 package com.savar_computer.healthassistants;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
