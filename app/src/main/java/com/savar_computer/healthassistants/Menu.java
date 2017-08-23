7a74d38e59505d6129f0788272829ade7d1e8cd8
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-23 14:59:09 +0430

    version 14

--- app/src/main/java/com/savar_computer/healthassistants/Menu.java
+++ app/src/main/java/com/savar_computer/healthassistants/Menu.java
@@ -11,16 +11,22 @@ public class Menu extends Activity {
     private ProgressBar progressBar;
 
     private TextView kalleryTxt;
+    private TextView sleeptime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
 
+        sleeptime=(TextView)findViewById(R.id.menu_sleep_time);
+        sleeptime.setText("24 hrs");
+
         kalleryTxt=(TextView)findViewById(R.id.menu_txt_kallery);
         kalleryTxt.setText("100 kcal");
+
         Typeface typeface=Typeface.createFromAsset(getAssets(),"Font/font.ttf");
         kalleryTxt.setTypeface(typeface);
+        sleeptime.setTypeface(typeface);
 
         progressBar=(ProgressBar)findViewById(R.id.menu_progress_bar);
         progressBar.setMax(100);
