0589dcbc5a70340e6b5263cdb32ba35bc6062272
ehsanyaz <yazdani.ehsan95@gmail.com>
2017-08-24 11:14:09 +0430

    ehsan version 25

--- app/src/main/java/com/savar_computer/healthassistants/AddFoodMenu.java
+++ app/src/main/java/com/savar_computer/healthassistants/AddFoodMenu.java
@@ -1,8 +1,12 @@
 package com.savar_computer.healthassistants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+
+import com.savar_computer.healthassistants.Classes.BlackBoard;
 
 public class AddFoodMenu extends AppCompatActivity {
 
@@ -12,5 +16,49 @@ public class AddFoodMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_food_menu);
+
+
+        food=(ImageView)findViewById(R.id.add_food_food);
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlackBoard.addFoodSelected= BlackBoard.FoodSelected.food;
+                Intent intent=new Intent(getApplicationContext(),addFood.class);
+                startActivity(intent);
+            }
+        });
+
+
+        fastFood=(ImageView)findViewById(R.id.add_food_fast_food);
+        fastFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlackBoard.addFoodSelected= BlackBoard.FoodSelected.fast_food;
+                Intent intent=new Intent(getApplicationContext(),addFood.class);
+                startActivity(intent);
+            }
+        });
+
+
+        drink=(ImageView)findViewById(R.id.add_food_drink);
+        drink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlackBoard.addFoodSelected= BlackBoard.FoodSelected.drink;
+                Intent intent=new Intent(getApplicationContext(),addFood.class);
+                startActivity(intent);
+            }
+        });
+
+
+        fruit=(ImageView)findViewById(R.id.add_food_fruit);
+        fruit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BlackBoard.addFoodSelected= BlackBoard.FoodSelected.fruit;
+                Intent intent=new Intent(getApplicationContext(),addFood.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/savar_computer/healthassistants/addFood.java
@@ -0,0 +1,15 @@
+package com.savar_computer.healthassistants;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class addFood extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_food);
+
+
+    }
+}
